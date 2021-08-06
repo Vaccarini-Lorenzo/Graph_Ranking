f5345ed15fd896cfdc0eda7a59d16f057c5c9287
Vaccarini-Lorenzo <lorenzomaria.vaccarini@mail.polimi.it>
2021-08-06 03:28:19 +0200

    up

--- Graph_Raker.c
+++ Graph_Raker.c
@@ -16,10 +16,12 @@ typedef struct {
     vertex* vertices;
     unsigned int size;
     unsigned int lenght;
+    // graph_node -> heap_node
+    int* graph_LUT;
 } binary_heap;
 
 unsigned int* matrix_computation(int d);
-binary_heap cheat_init_heap(int d); // inits a heap with min 0 and other -1 (infty);   O(N)
+binary_heap quick_init_heap(int d); // inits a heap with min 0 and other -1 (infty);   O(N)
 int parent(int node_index);
 int left(binary_heap* heap, int vertex);
 int right(binary_heap* heap, int vertex);
@@ -44,12 +46,12 @@ int main(){
             // O(N^2)
             adjacence_matrix = matrix_computation(d);
             dijkstra(d, adjacence_matrix);
-
         }
     }
     return 0;
 }
 
+// FIX THIS -> LINKED LIST
 unsigned int* matrix_computation(int d){
     unsigned int* adjacence_matrix = (unsigned int*)malloc(sizeof(unsigned int) * d * d );
     int buffer;
@@ -68,17 +70,21 @@ unsigned int* matrix_computation(int d){
     return adjacence_matrix;
 }
 
-binary_heap cheat_init_heap(int d){
+binary_heap quick_init_heap(int d){
     binary_heap heap;
+    heap.vertices = (vertex*)malloc(sizeof(vertex) * heap.size);
+    heap.graph_LUT = (int*)malloc(sizeof(int) * (d + 1));
+
     heap.lenght = d;
     heap.size = d + 1;
-    heap.vertices = (vertex*)malloc(sizeof(vertex) * heap.size);
     heap.vertices[0].distance = 0;
     heap.vertices[0].id = 0;
+    heap.graph_LUT[0] = 0;
 
     for(unsigned int i = 1; i  < d; i++){
         heap.vertices[i].distance = INFTY;
         heap.vertices[i].id = i;
+        heap.graph_LUT[i] = i;
     }
     return heap;
 }
@@ -109,19 +115,33 @@ int right(binary_heap* heap, int node_index){
 
 // index_0: a node_index (index from the array of distances, see vertex structure)
 void swap(binary_heap* heap, int index_0, int index_1){
-    unsigned int tmp = heap->vertices[index_0].distance;
+    printf("index_0 = %d, index_1 = %d\n", index_0, index_1);
+    printf("LUT status BEFORE SWAP:\n");
+    for(int i = 0; i < 4; i++){
+        printf("[%d] = %d\n", i, heap->graph_LUT[i]);
+    }
+    unsigned int tmp_dist = heap->vertices[index_0].distance;
+    int tmp_id = heap->vertices[index_0].id;
+    int heap_index = heap->graph_LUT[index_1];
+    vertex v = heap->vertices[heap_index];
+    // LUT update
+    heap->graph_LUT[index_0] =  v.id;
+    heap->graph_LUT[index_1] = tmp_id;
+    printf("LUT status AFTER swap:\n");
+    for(int i = 0; i < 4; i++){
+        printf("[%d] = %d\n", i, heap->graph_LUT[i]);
+    }
+     // Vertex update
     heap->vertices[index_0].distance = heap->vertices[index_1].distance;
-    heap->vertices[index_1].distance = tmp;
+    heap->vertices[index_0].id = heap->vertices[index_1].id;
+    heap->vertices[index_1].distance = tmp_dist;
+    heap->vertices[index_1].id = tmp_id;
 }
 
 // Min_heapify: from the node_index (index from the array of distances) recursive check of the tree property
 void min_heapify(binary_heap* heap, int node_index){
     int l = left(heap, node_index);
     int r = right(heap, node_index);
-    // DEBUGGING PURPOSES
-    unsigned int head_distance = heap->vertices[node_index].distance;
-    unsigned int left_distance = heap->vertices[l].distance;
-    unsigned int right_distance = heap->vertices[r].distance;
 
     int next_check;
     if(l > node_index && heap->vertices[l].distance < heap->vertices[node_index].distance)
@@ -138,8 +158,11 @@ void min_heapify(binary_heap* heap, int node_index){
 
 vertex remove_min(binary_heap* heap){
     vertex min = heap->vertices[0];
+    int tmp_index = heap->graph_LUT[heap->lenght - 1];
     heap->lenght--;
     heap->size--;
+    heap->graph_LUT[heap->lenght] = heap->graph_LUT[0];
+    heap->graph_LUT[0] = tmp_index;
     heap->vertices[0] = heap->vertices[heap->lenght];
     min_heapify(heap, 0);
     return min;
@@ -151,11 +174,14 @@ void update_heap(binary_heap* heap, int node_index){
         swap(heap, node_index, check);
         check = parent(check);
     }
+
 }
 
 void decrease_distance(binary_heap* heap, int id, unsigned int lower_distance){
-    vertex* v;
+    int heap_index = heap->graph_LUT[id];
+    vertex* v = &heap->vertices[heap_index];
     // THIS HAS TO BE OPTIMIZED
+    /*
     for(int i = 0; i < heap->lenght; i++){
         if(heap->vertices[i].id == id){
             v = &heap->vertices[i];
@@ -163,6 +189,9 @@ void decrease_distance(binary_heap* heap, int id, unsigned int lower_distance){
             update_heap(heap, i);
         }
     }
+    */
+    v->distance = lower_distance;
+    update_heap(heap, heap_index);
 }
 
 int is_empty(binary_heap* heap){
@@ -173,33 +202,26 @@ int is_empty(binary_heap* heap){
 
 // Maybe adjacence_matrix is not a good idea -- Need to fix this
 void dijkstra(int d, unsigned int* adjacence_matrix){
-    binary_heap heap = cheat_init_heap(d);
+    binary_heap heap = quick_init_heap(d);
     int* paths_cost = (int*)malloc(sizeof(int) * (d+1));
     int cost;
     int node;
+    int neighbour_index;
     vertex* neighbour_v;
     while(!is_empty(&heap)){
         vertex v = remove_min(&heap);
-        //printf("lenght = %d\n", heap->lenght);
         node = v.id;
         paths_cost[node] = v.distance;
         for(int i = 0; i < d; i++){
             if(adjacence_matrix[node * d + i] != 0){
                 cost = adjacence_matrix[node * d + i];
-                // FIX -> Too expensive! Table look-up
-                for(int j = 0; j < heap.lenght; j++){
-                    if(heap.vertices[j].id == i){
-                        neighbour_v = &heap.vertices[j];
-                    }
-                }
-                if(neighbour_v->distance > (cost + v.distance)){
+                neighbour_index = heap.graph_LUT[i];
+                // What happens here?
+                neighbour_v = &heap.vertices[neighbour_index];
+                if(neighbour_v->id == i && neighbour_v->distance > (cost + v.distance)){
                     decrease_distance(&heap, i, cost + v.distance);
                 }
-                printf("Actual state of the heap:\n");
-                for(int i = 0; i < heap.lenght; i++){
-                    printf("[H%d] = %u\n", i, heap.vertices[i].distance);
-                }
-                sleep(1);
+
             }
         }
     }
